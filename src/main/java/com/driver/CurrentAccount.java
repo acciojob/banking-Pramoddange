bb3987a5595a837144c06715d2efea61ab642fc9
Pramod Dange <pramodsdange1@gmail.com>
2024-01-28 23:39:07 +0530

    done Banking Assig

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,19 +1,70 @@
 package com.driver;
 
+import java.util.Arrays;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
+
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
-    }
+        super(name, balance,5000); // Call the constructor of the parent class (BankAccount)
+
+        // Minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        if (balance < 5000) {
+            throw new Exception("Insufficient Balance");
+        }
+
+        this.tradeLicenseId = tradeLicenseId;
+            }
 
     public void validateLicenseId() throws Exception {
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        // Check if no two consecutive characters are the same
 
+        if (!hasConsecutiveCharacters(tradeLicenseId)) {
+            return; // Valid license ID, do nothing
+        }
+
+        // Check if characters can be rearranged to create a valid license ID
+        if (canRearrangeToValidId(tradeLicenseId)) {
+            return; // Valid license ID, do nothing
+        }
+
+        // If the conditions are not met, throw an exception
+        throw new Exception("Valid License can not be generated");
     }
+
+    private boolean hasConsecutiveCharacters(String licenseId) {
+        for (int i = 0; i < licenseId.length() - 1; i++) {
+            if (licenseId.charAt(i) == licenseId.charAt(i + 1)) {
+                return true; // Consecutive characters are the same
+            }
+        }
+        return false; // No consecutive characters are the same
+    }
+
+    private boolean canRearrangeToValidId(String licenseId) {
+        // Convert the licenseId to an array of characters for easier manipulation
+        char[] charArray = licenseId.toCharArray();
+
+        // Sort the array to group similar characters together
+        Arrays.sort(charArray);
+
+        // Check if the sorted array has consecutive characters
+        for (int i = 0; i < charArray.length - 1; i++) {
+            if (charArray[i] == charArray[i + 1]) {
+                return true; // Consecutive characters are the same
+            }
+        }
+
+        return false; // No consecutive characters are the same after sorting
+    }
+
+
 
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -6,22 +6,39 @@ public class SavingsAccount extends BankAccount{
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
+        super(name, balance, 0); // minimum balance is 0 by default
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        this.rate = rate;
 
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
+        if (amount > maxWithdrawalLimit) {
+            throw new Exception("Maximum Withdraw Limit Exceeded");
+        }
 
+        // Check if the withdrawal amount exceeds the balance
+        if (amount >getBalance()) {
+            throw new Exception("Insufficient Balance");
+        }
+
+        // Perform the withdrawal
+        super.withdraw(amount);
     }
+
+
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-
+        double interest = getBalance() * rate * years / 100;
+        return getBalance() + interest;
     }
-
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
+        double compoundInterest = getBalance() * Math.pow((1 + rate / (times * 100)), times * years);
+        return compoundInterest;
 
     }
 
